d5ab9c6e589fad3c1f8622ec161ac5dfa5cfceb4
Skantastico <Skantastico@gmail.com>
2020-11-08 21:38:02 -0500

    got ex1 tests passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,30 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # make an empty cache
+    cache = {}
 
-    return None
+    # storing the weights with index as value
+
+    for k, v in enumerate(weights):
+        cache[v] = k
+
+    # find the indicies that sum up to the limit value
+
+    for k, w in enumerate(weights):
+        # check if limit - weight is in cache
+        if (limit - w) in cache:
+            # if it is, and is greater than k, return answer as tuple
+            if cache[limit-w] > k:
+                return (cache[limit-w], k)
+
+            # if it is not greater, flip the tuple
+            else:
+                return (k, cache[limit-w])
+
+
+# # for testing
+#
+# wt = [ 4, 6, 10, 15, 16 ]
+# len = 5
+# lim = 16
+#
+# print(get_indices_of_item_weights(wt, len, lim))
